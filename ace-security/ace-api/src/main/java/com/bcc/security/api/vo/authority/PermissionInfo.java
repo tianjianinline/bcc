9746b85abcf47cd2d3f3db2c683f2db77d02c576
tianjian <tianjian@primeton.com>
2018-05-08 13:26:31 +0800

    commit

--- ace-security/ace-api/src/main/java/com/bcc/security/api/vo/authority/PermissionInfo.java
+++ ace-security/ace-api/src/main/java/com/bcc/security/api/vo/authority/PermissionInfo.java
@@ -2,67 +2,22 @@ package com.bcc.security.api.vo.authority;
 
 import java.io.Serializable;
 
+import lombok.Data;
+
 /**
  * ${DESCRIPTION}
  *
  * @author wanghaobin
  * @create 2017-06-22 15:19
  */
+@Data
 public class PermissionInfo implements Serializable{
-    private String code;
+	private static final long serialVersionUID = 8298241774592446243L;
+	private String code;
     private String type;
     private String uri;
     private String method;
     private String name;
     private String menu;
 
-    public String getMenu() {
-        return menu;
-    }
-
-    public void setMenu(String menu) {
-        this.menu = menu;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
 }
--- ace-security/ace-api/src/main/java/com/bcc/security/api/vo/user/UserInfo.java
+++ ace-security/ace-api/src/main/java/com/bcc/security/api/vo/user/UserInfo.java
@@ -1,7 +1,8 @@
 package com.bcc.security.api.vo.user;
 
 import java.io.Serializable;
-import java.util.Date;
+
+import lombok.Data;
 
 /**
  * ${DESCRIPTION}
@@ -9,60 +10,12 @@ import java.util.Date;
  * @author wanghaobin
  * @create 2017-06-21 8:12
  */
+@Data
 public class UserInfo implements Serializable{
-    public String id;
+	private static final long serialVersionUID = -8175097928216609824L;
+	public String id;
     public String username;
     public String password;
     public String name;
     private String description;
-
-    public Date getUpdTime() {
-        return updTime;
-    }
-
-    public void setUpdTime(Date updTime) {
-        this.updTime = updTime;
-    }
-
-    private Date updTime;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
