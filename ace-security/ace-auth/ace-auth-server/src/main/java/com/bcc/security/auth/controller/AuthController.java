4e40c35b950556b10bd4554a43dc1bc2f3d75eab
tianjian <tianjian@primeton.com>
2018-05-09 15:57:57 +0800

    1

--- ace-security/ace-auth/ace-auth-server/src/main/java/com/bcc/security/auth/controller/AuthController.java
+++ ace-security/ace-auth/ace-auth-server/src/main/java/com/bcc/security/auth/controller/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bcc.security.auth.common.util.jwt.JWTInfo;
