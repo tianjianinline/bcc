3b4ef68ee398acc4688ef3f342113a8e317acf10
tianjian <tianjian@primeton.com>
2018-05-09 16:56:28 +0800

    1

--- ace-security/ace-auth/ace-auth-client/src/main/java/com/bcc/security/auth/client/feign/ServiceAuthFeign.java
+++ ace-security/ace-auth/ace-auth-client/src/main/java/com/bcc/security/auth/client/feign/ServiceAuthFeign.java
@@ -5,10 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.bcc.security.auth.common.util.jwt.JWTInfo;
-import com.bcc.security.common.msg.ObjectRestResponse;
-
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.SignatureException;
 
@@ -17,11 +14,7 @@ import io.jsonwebtoken.SignatureException;
  */
 @FeignClient(value = "${auth.serviceId}",configuration = {})
 public interface ServiceAuthFeign {
-    @RequestMapping(value = "/client/token",method = RequestMethod.POST)
-    public ObjectRestResponse<String> getAccessToken(@RequestParam("clientId") String clientId, @RequestParam("secret") String secret);
-    @RequestMapping(value = "/client/userPubKey",method = RequestMethod.POST)
-    public ObjectRestResponse<byte[]> getUserPublicKey(@RequestParam("clientId") String clientId, @RequestParam("secret") String secret);
-    
+
     @RequestMapping(value = "/jwt/verify",method = RequestMethod.GET)
     public ResponseEntity<Boolean> isValid(@RequestParam("token") String token) throws ExpiredJwtException,SignatureException;
     
